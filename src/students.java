d148426a912df65e072215a48e36c6b6ff6fe862
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2022-03-10 16:24:30 +0100

    Added more humor :)

--- src/students.java
+++ src/students.java
@@ -1,7 +1,7 @@
 public class students {
     public static void main(String[] args) {
         String student = "TimTomKat";
-        System.out.printf("%50s", student);
+        System.out.printf("%500s", student);
 
     }
 }
